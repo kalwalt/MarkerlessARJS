973ae526ade3e8bdf9385b95699d9826227d04ad
Walter Perdan <github@kalwaltart.it>
2023-12-06 18:20:11 +0100

    switch to a main solution

--- build_em.sh
+++ build_em.sh
@@ -5,4 +5,4 @@
 docker exec emscripten-markerlessarjs emcmake cmake -B build_em -S ./ -DEMSCRIPTEN_COMP=1 -DCMAKE_BUILD_TYPE=Release ..
 docker exec emscripten-markerlessarjs emmake make -C build_em
 
-cp build_em/MarkerlessARJS.js build/lib
+cp build_em/MarkerlessARJS.html build/lib
