174ada770b14670ccd0644b553f3912bbac72621
kalwalt <github@kalwaltart.it>
2024-02-06 18:25:01 +0100

    testing igl compilation with emscripten

--- build_em.sh
+++ build_em.sh
@@ -2,7 +2,9 @@
 
 #docker run -dit --name emscripten-markerlessarjs -v $(pwd):/src emscripten/emsdk:3.1.26 bash
 
-docker exec emscripten-markerlessarjs emcmake cmake -B build_em -S ./ -DEMSCRIPTEN_COMP=1 -DCMAKE_BUILD_TYPE=Release ..
+#cp data/pinball.jpg build_em
+
+docker exec emscripten-markerlessarjs emcmake cmake -B build_em -S ./ -DEMSCRIPTEN_COMP=1 -DIGL_DEPLOY_DEPS=OFF -DCMAKE_BUILD_TYPE=Release ..
 docker exec emscripten-markerlessarjs emmake make -C build_em
 
 cp build_em/MarkerlessARJS.html build/lib
