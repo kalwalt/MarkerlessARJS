6df0002a825b49c03334ed2618aec622cfe21705
Walter Perdan <github@kalwaltart.it>
2024-02-11 16:50:08 +0100

    different builds script

---
+++ build_w_window_em.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#docker run -dit --name emscripten-markerlessarjs -v $(pwd):/src emscripten/emsdk:3.1.26 bash
+
+cd build_em
+mkdir data
+cd ..
+cp data/igl.html build_em/data
+cp data/pinball.jpg build_em/data
+
+emcmake cmake -G "Ninja" -DEMSCRIPTEN_COMP=1 -DCMAKE_BUILD_TYPE=Release ..
+emmake make -C build_em
+
+cp build_em/MarkerlessARJS.html build/lib
