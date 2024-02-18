083b57450836c5d8f90f9a12c2e28f8cd596e5b1
Walter Perdan <github@kalwaltart.it>
2024-02-18 19:38:55 +0100

    sphere is displayed - new buildProjectionMatrix function but with issues

--- build_w_window_em.sh
+++ build_w_window_em.sh
@@ -3,10 +3,6 @@
 #docker run -dit --name emscripten-markerlessarjs -v $(pwd):/src emscripten/emsdk:3.1.26 bash
 
 cd build_em
-mkdir data
-cd ..
-cp data/igl.html build_em/data
-cp data/pinball.jpg build_em/data
 
 emcmake cmake -G "Ninja" -DEMSCRIPTEN_COMP=1 -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
