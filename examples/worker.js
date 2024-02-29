0122a595d59620058dc8943de77f8116a40ece35
Walter Perdan <github@kalwaltart.it>
2024-02-29 18:38:12 +0100

    now blue rect is displayed, but it is scrambled - forgot to JSON.parse corners data

--- examples/worker.js
+++ examples/worker.js
@@ -1,18 +1,18 @@
 importScripts("../build/lib/MarkerlessARJS.js");
 
 var pinball_buff;
-var stream, loc, corners;
+var stream, loc, corners, videoWidth, videoHeight;
 
 // Create a new instance of the MarkerlessARJS class
 Module.onRuntimeInitialized = async function () {
-    var cc = new Module.CameraCalibration(4000, 3000, 2000, 1500);
+    var cc = new Module.CameraCalibration(640, 480, 320, 240);
     console.log(cc);
-    var projMat = Module.buildProjectionMatrix(cc, 0.01, 1000.0, 4000, 3000);
+    var projMat = Module.buildProjectionMatrix(cc, 0.01, 1000.0, 640, 480);
     console.log(projMat);
     var pipeline = new Module.ARPipeline(1637, 2048, pinball_buff, cc);
     console.log(pipeline);
     function update() {
-        var ok = pipeline.processFrame(stream, 640, 480);
+        var ok = pipeline.processFrame(stream, videoWidth, videoHeight);
         console.log(ok);
         loc = pipeline.getPatternMat44();
         corners = pipeline.getCorners();
@@ -32,5 +32,7 @@ self.onmessage = function (ev) {
     }
     if (msg.type === "video_data") {
         stream = msg.data;
+        videoWidth = msg.videoWidth;
+        videoHeight = msg.videoHeight;
     }
 }
