6b3dbc66b9ecbd089ef599bde88cf420c1765402
kalwalt <github@kalwaltart.it>
2024-03-23 20:44:51 +0100

    cleaning the code

--- examples/worker.js
+++ examples/worker.js
@@ -1,42 +1,11 @@
 importScripts("../dist/MarkerlessAR.js");
 
-console.log(MarkerlessAR);
-const m = new MarkerlessAR.default.MarkerlessAR();
-console.log(m);
-//const markerless = await MarkerlessAR.default.MarkerlessAR.init();
-//console.log(markerless);
-/*MarkerlessAR.default.MarkerlessAR.init().then((markerless) => {
-    console.log(markerless);
-})*/
 var markerless;
 var cc, pipeline, projMat;
 var pinball_buff;
 var stream = null;
 var loc, corners, videoWidth, videoHeight;
 
-/*self.onmessage = function (ev) {
-    var msg = ev.data;
-    if (msg.type === "pinball") {
-        pinball_buff = msg.buff;
-        load(pinball_buff);
-    }
-    if (msg.type === "video_data") {
-        stream = msg.data;
-        videoWidth = msg.videoWidth;
-        videoHeight = msg.videoHeight;
-        update();
-    }
-    if (msg.type === "test") {
-        console.log("test");
-    }
-}*/
-
-/*MarkerlessAR.default.MarkerlessAR.init().then((markerless) => {
-    console.log(markerless);
-    cc = new markerless.CameraCalibration(1280, 720, 640, 360);
-    console.log(cc);
-})
-*/
 self.onmessage = function (e) {
     var msg = e.data;
     switch (msg.type) {
@@ -66,7 +35,7 @@ function initTracker(buff) {
     function onLoad(m) {
         markerless = m;
         console.log('Starting the pipeline setup...');
-        cc = new m.CameraCalibration(1280, 720, 640, 360);
+        cc = new m.CameraCalibration(640, 480, 320, 240);
         console.log(cc);
         projMat = m.buildProjectionMatrix(cc, 0.01, 1000.0, 640, 480);
         console.log(projMat);
