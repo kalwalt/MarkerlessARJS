47b1049af76b3fb33e0c16b770236dacd3edac12
kalwalt <github@kalwaltart.it>
2024-03-23 19:31:37 +0100

    new dist library with webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+  let devtool = false;
+  if (argv.mode === "development") {
+    devtool = "inline-source-map";
+  }
+  console.log(`${argv.mode} build`);
+  const module = {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                // @see https://github.com/babel/babel/issues/9849
+                ["@babel/transform-runtime"],
+              ],
+            },
+          },
+         
+        ],
+      },
+    ],
+  };
+  return [
+    {
+      name: "markerless",
+      devtool,
+      entry: "./src/index.js",
+      output: {
+        path: path.resolve('dist'),
+        filename: 'MarkerlessAR.js',
+        library: 'MarkerlessAR',
+        libraryTarget: 'umd',
+        // @see: https://github.com/webpack/webpack/issues/3929
+        //libraryExport: 'default',
+        // @see: https://github.com/webpack/webpack/issues/6522
+        globalObject: 'typeof self !== \'undefined\' ? self : this',
+      },
+      resolve: {
+        extensions: [".js"],
+        // @see https://stackoverflow.com/questions/59487224/webpack-throws-error-with-emscripten-cant-resolve-fs
+        fallback: {
+          fs: false,
+          path: false,
+          crypto: false,
+        },
+      },
+      module,
+    },
+  ];
+};
