38d01cafbc0ffed69972abc6b345983593c8ef9f
kalwalt <github@kalwaltart.it>
2024-03-27 16:25:52 +0100

    removing html example

--- build_em.sh
+++ build_em.sh
@@ -2,7 +2,7 @@
 
 #docker run -dit --name emscripten-markerlessarjs -v $(pwd):/src emscripten/emsdk:3.1.26 bash
 
-docker exec emscripten-markerlessarjs emcmake cmake -B build_em -S ./ -DEMSCRIPTEN_COMP=1 -DHTML_LIB=0 -DCMAKE_BUILD_TYPE=Release ..
+docker exec emscripten-markerlessarjs emcmake cmake -B build_em -S ./ -DEMSCRIPTEN_COMP=1 -DCMAKE_BUILD_TYPE=Release ..
 docker exec emscripten-markerlessarjs emmake make -C build_em
 
 cp build_em/MarkerlessARJS.html build/lib
