7bd700b13587d910fee77701fe6b8d37463b5a6a
kalwalt <github@kalwaltart.it>
2024-03-27 16:06:19 +0100

    better msg handling for video size and pattern size

--- examples/worker.js
+++ examples/worker.js
@@ -5,13 +5,14 @@ var cc, pipeline, projMat;
 var pinball_buff;
 var stream = null;
 var loc, corners, videoWidth, videoHeight;
+var vWidth, vHeight;
 
 self.onmessage = function (e) {
     var msg = e.data;
     switch (msg.type) {
       case "pinball": {
         pinball_buff = msg.buff;
-        initTracker(pinball_buff);
+        initTracker(pinball_buff, msg);
         return;
       }
       case "video_data": {
@@ -31,20 +32,24 @@ self.addEventListener('message', function(e) {
     self.postMessage(e.data);
     }, false);
 
-function initTracker(buff) {
+function initTracker(buff, msg) {
     function onLoad(m) {
         markerless = m;
         console.log('Starting the pipeline setup...');
-        cc = new m.CameraCalibration(320, 240, 160, 120);
+        vWidth = msg.vSizeWidth;
+        vHeight = msg.vSizeHeight;
+        let patternWidth = msg.width;
+        let patternHeight = msg.height;
+        cc = new m.CameraCalibration(vWidth, vHeight, vWidth/2, vHeight/2);
         console.log(cc);
-        projMat = m.buildProjectionMatrix(cc, 0.01, 1000.0, 320, 240);
+        projMat = m.buildProjectionMatrix(cc, 0.01, 1000.0, vWidth/2, vHeight/2);
         console.log(projMat);
         if(projMat) {
             self.postMessage({type: "projection_matrix", projMat: JSON.stringify(projMat)});
         }
         console.log('loading the pinball image...');
         if(buff) {
-            pipeline = new m.ARPipeline(1637, 2048, buff, cc);
+            pipeline = new m.ARPipeline(patternWidth, patternHeight, buff, cc);
             pipeline.refineHomography(true);
             console.log(pipeline);
         }
@@ -59,7 +64,7 @@ function update() {
     var ok;
     if (pipeline && pipeline.processFrame) {
 
-        ok = pipeline.processFrame(stream.data, 320, 240);
+        ok = pipeline.processFrame(stream.data, vWidth, vHeight);
 
     }
     console.log("marker found: ", ok);
